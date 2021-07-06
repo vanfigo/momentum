18a3b6dff359d37e5c5aa92329b86dabea2c9508
vanfigo <rloyolaj@gmail.com>
2021-07-05 22:11:07 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'momentum',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
